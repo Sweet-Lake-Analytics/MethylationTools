f8c611d0f7a2d06c91862d3e4c21af71352791db
Chris Jacobs <cgc.jacobs@pm.me>
2025-04-02 11:46:26 +0200

    Adding sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,32 @@
-# Placeholder
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Methylation Tools'
+copyright = '2025, Sweet Lake Analytics'
+author = 'C.G.C. Jacobs'
+release = 'v1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static/style.css']
+html_logo = '_static/SL_logo.png'
+html_theme_options = {
+    'style_nav_header_background': 'grey'
+}
